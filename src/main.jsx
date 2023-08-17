212cceab37b26cbc7a895cd09c8f0554e6d90c43
EmySilv <emiisilva1978@gmail.com>
2023-08-16 21:37:59 -0300

    Inicializando o projeto

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './pages/home/home.jsx'
+import './global/styles.global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="macacopeludo">
+            <h1>EU TE AMO MACACO PELUDO S2</h1>
+        </div>
+    );
+}
+
+export default Home;

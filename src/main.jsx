c544a24f0aeb92b2165be1feba1a1d55df022292
Salutx <gentiugente@gmail.com>
2023-08-24 22:12:46 -0300

    feat(Emilly): Static Login

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global/styles.global.css'
+import 'remixicon/fonts/remixicon.css'
 import Authentication from './pages/Authentication/authentication'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,75 +1,80 @@
 import React from 'react'
+import * as Style from "./Authentication.styles";
+import EASYPROG_LOGO from "../../assets/images/easyprog-logo.svg"
+import EASYPROG_ROBOT from "../../assets/images/easybot.svg"
 
 const Authentication = () => {
-    // JavaScript
+    // Insert javascript here
     return (
-        <div class="pai-vermelho">
-            <div class="filho-azul">
-                <div class="filho-verde">
-                    <div class="filho-marrom">
-                        <img src="" alt="logo EasyProg" />
-                    </div>
-                    <div class="filho-ciano">
-                        <p>EASYPROG</p>
-                        <p>A Testing Framework</p>
-                    </div>
-                </div>
-                <div class="filho-laranja">
-                    <div class="filho-azulClaro">
-                        <div class="filho-verdeOliva">
-                            <img src="" alt="EasyBot" />
-                            <h1>Authentication</h1>
-                            <p>Autentique-se na plataforma para utilizar as <strong>ferramentas</strong>.</p>
-                        </div>
-                        <div class="filho-amarelo">
-                            <div class="input-wrapper">
-                                <label htmlFor='username'>Username</label>
-                                <div class="input-item">
-                                    <img src="" alt="Icone Username" />
-                                    <input type="text" placeholder='User' id='username' />
-                                </div>
-                            </div>
-                            <div class="input-wrapper">
-                                <label htmlFor='password'>Senha</label>
-                                <div class="input-item">
-                                    <img 
-                                        src="" 
-                                        alt="Icone Password" 
-                                    />
-                                    <input 
-                                        type="text" 
-                                        placeholder='User' 
-                                        id='password' 
-                                    />
-                                    <img 
-                                        src="" 
-                                        alt="Icone ViewPassword" 
-                                    />
-                                </div>
-                            </div>
-                            <div class="filho-esmeralda">
-                                <input 
-                                    type="checkbox" 
-                                    name='KeepConnected' 
-                                    id='KeepConnected' 
-                                />
-                                <label htmlFor="KeepConnected">
-                                    Manter-se conectado
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="filho-verdeLimon">
-                        <button>
-                            Cadastrar-se
-                        </button>
-                        <button>
-                            Continuar
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Style.Main>
+            <Style.Container>
+                <Style.AuthenticationWrapper>
+                    <Style.AuthenticationBackground>
+                        <Style.Form>
+                            <Style.FormHeader>
+                                <Style.FormLogo >
+                                    <img src={EASYPROG_LOGO} alt="logo EasyProg" />
+                                </Style.FormLogo>
+                                <Style.FormHeaderText>
+                                    <p>EASYPROG</p>
+                                    <p>A Testing Framework</p>
+                                </Style.FormHeaderText>
+                            </Style.FormHeader>
+                            <Style.FormWrapper>
+                                <Style.FormAuthentication>
+                                    <Style.FormAuthenticationText>
+                                        <img src={EASYPROG_ROBOT} alt="EasyBot" />
+                                        <h1>Authentication</h1>
+                                        <p>Autentique-se na plataforma para utilizar as <strong>ferramentas</strong>.</p>
+                                    </Style.FormAuthenticationText>
+                                    <Style.FormBody>
+                                        <Style.InputWrapper>
+                                            <Style.InputLabel htmlFor='username'>Username</Style.InputLabel>
+                                            <Style.InputItem>
+                                                <i class="ri-user-line"></i>
+                                                <input type="text" placeholder='User' id='username' />
+                                            </Style.InputItem>
+                                        </Style.InputWrapper>
+                                        <Style.InputWrapper>
+                                            <Style.InputLabel htmlFor='password'>Senha</Style.InputLabel>
+                                            <Style.InputItem>
+                                                <i class="ri-lock-2-line"></i>
+                                                <input
+                                                    type="text"
+                                                    placeholder='User'
+                                                    id='password'
+                                                />
+                                                <i class="ri-eye-line"></i>
+                                            </Style.InputItem>
+                                        </Style.InputWrapper>
+                                        <Style.FormCheck>
+                                            <input
+                                                type="checkbox"
+                                                name='KeepConnected'
+                                                id='KeepConnected'
+                                            />
+                                            <label htmlFor="KeepConnected">
+                                                <i class="ri-check-line"></i>
+                                            </label>
+                                            <p>Manter-se conectado</p>
+                                        </Style.FormCheck>
+                                    </Style.FormBody>
+                                </Style.FormAuthentication>
+                                <Style.FormButtons>
+                                    <Style.ButtonRegister>
+                                        Cadastrar-se
+                                    </Style.ButtonRegister>
+                                    <Style.ButtonContinue>
+                                        Continuar
+                                    </Style.ButtonContinue>
+                                </Style.FormButtons>
+                            </Style.FormWrapper>
+                        </Style.Form>
+                    </Style.AuthenticationBackground>
+                </Style.AuthenticationWrapper>
+            </Style.Container>
+            <Style.MainBackground/>
+        </Style.Main>
     )
 }
 

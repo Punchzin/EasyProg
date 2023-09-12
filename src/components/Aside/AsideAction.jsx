b6084d45ac466aa9bd04c7a435af9453e415c3cf
EmySilv <emiisilva1978@gmail.com>
2023-09-11 21:20:36 -0300

    feath... added action for buttons

--- src/components/Aside/AsideAction.jsx
+++ src/components/Aside/AsideAction.jsx
@@ -1,14 +1,14 @@
 import * as Style from "./Aside.styles.jsx";
 
 // Props = Propriedades
-const AsideAction = ({ actionIcon, actionTitle, actionIsSelected }) => {
+const AsideAction = ({ actionIcon, actionTitle, actionIsSelected, onClick }) => {
     return (
         actionIsSelected ? (
-            <Style.AsideActionSelected title={actionTitle}>
+            <Style.AsideActionSelected title={actionTitle} onClick={onClick}>
                 <i className={actionIcon}></i>
             </Style.AsideActionSelected>
         ) : (
-            <Style.AsideAction title={actionTitle}>
+            <Style.AsideAction title={actionTitle} onClick={onClick}>
                 <i className={actionIcon}></i>
             </Style.AsideAction>
         )

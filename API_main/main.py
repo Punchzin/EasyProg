adf7a35772ce14c9e4525114c8e0f402b9f20287
Punchzin <gabrielssscavalcante@gmail.com>
2023-09-13 23:54:46 +0000

    a

--- API_main/main.py
+++ API_main/main.py
@@ -1,7 +1,7 @@
 import openai
 import json
 
-API_KEY = "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"
+API_KEY = "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"
 openai.api_key = API_KEY
 
 # Opens the json file with incorrect code
@@ -11,39 +11,37 @@ with open('incorrect_code.json', 'r') as f:
 # Get the code
 code = data['incorrect_code']
 
+prompt = code
 
-if is_python_code(user_input):
-    # Call the GPT-4 API with the user's code
-    prompt = user_input
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": "You will be provided with a piece of Python code, and your task is to find and fix bugs in it."
+        },
+        {
+            "role": "user",
+            "content": prompt
+        },
+    ],
+    temperature=0.5,
+    max_tokens=1024
+)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {
-                "role": "system",
-                "content": "You will be provided with a piece of Python code, and your task is to find and fix bugs in it."
-            },
-            {
-                "role": "user",
-                "content": prompt
-            },
-        ],
-        temperature=0,
-        max_tokens=1024
-    )
+# Extract the corrected code from the response
+corrected_code = response['choices'][0]['message']['content']
 
-    # Extract the corrected code from the response
-    corrected_code = response['choices'][0]['message']['content']
-    # Save the corrected code in a JSON file
-    data = {"corrected_code": corrected_code}
-    
-    with open('corrected_code.json', 'w') as file:
-        json.dump(data, file, indent=4)
-    
-    # Read the response from the JSON file
-        with open('corrected_code.json', 'r') as f:
-          data = json.load(f)
-    
-    # Get the corrected code content
-    content = data['corrected_code']
-    print(corrected_code)
+# Save the corrected code in a JSON file
+data = {"corrected_code": corrected_code}
+
+with open('corrected_code.json', 'w') as file:
+    json.dump(data, file, indent=4)
+
+# Read the response from the JSON file
+with open('corrected_code.json', 'r') as f:
+    data = json.load(f)
+
+# Get the corrected code content
+content = data['corrected_code']
+print(content)

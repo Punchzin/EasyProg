948647ec8dc3ea0059918bf41e554916e4970022
Punchzin <gabrielssscavalcante@gmail.com>
2023-10-11 18:52:42 +0000

    front ajustado e back mudanças

--- API/main.py
+++ API/main.py
@@ -1,59 +1,50 @@
-
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import openai
 import mysql.connector
-
-app = Flask(__name__)
-
-# Database config
-db_config = {
-    "host": "cluster.cdxnv8oe5mq3.sa-east-1.rds.amazonaws.com",
-    "port": "3306",
-    "user": "admin",
-    "password": "easyprogefoda",
-    "database": "easyprog"
-}
+import flask.py
 
 # OpenAI GPT-3.5-turbo API credentials
-openai.api_key = "sk-4XLAc2lya6fw89Hzh04IT3BlbkFJahB7tLSIrmfk"
+openai.api_key = "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"
+# Database config
+db_config = {
+            "host": "cluster.cdxnv8oe5mq3.sa-east-1.rds.amazonaws.com",
+            "port": "3306",
+            "user": "admin",
+            "password": "easyprogefoda",
+            "database": "easyprog"
+    }
 
 # Function to correct Python code using OpenAI GPT-3.5-turbo API
-def correct_python_code(code):
+def correct_python_code(incorrect_code):
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=incorrect_code,
+        prompt= "correct the following python code: " incorrect_code,
         temperature=0.6,
-        max_tokens=100,
+        max_tokens=600,
         top_p=1.0,
         n=1,
         stop=None,
     )
-    corrected_code = response.choices[0].text.strip()
-    return corrected_code
+correct_python_code()
 
-@app.route('/api/send', methods=['POST'])
-def receive_code():
-    # Get the incorrect Python code from the user
-    incorrect_code = request.json.get('data')
-    
-    # MySQL connection
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
+corrected_code = response.choices[0].text.strip()
+return corrected_code
+print(corrected_code)
 
-    # Correct the Python code using OpenAI GPT-3.5-turbo API
-    corrected_code = correct_python_code(incorrect_code)
+# MySQL connection
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
 
-    # Save the incorrect and corrected code to the database
-    query = "INSERT INTO inputs (question, answer) VALUES (%s, %s)"
-    values = (incorrect_code, corrected_code)
-    cursor.execute(query, values)
-    conn.commit()
+# Correct the Python code using OpenAI GPT-3.5-turbo API
+corrected_code = main.correct_python_code(incorrect_code)
 
-    # Close the MySQL connection
-    cursor.close()
-    conn.close()
+ # Save the incorrect and corrected code to the database
+query = "INSERT INTO inputs (question, answer) VALUES (%s, %s)"
+values = (incorrect_code, corrected_code)
+cursor.execute(query, values)
+conn.commit()
 
-    return {"message": "Code received and corrected"}
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000) 
+# Close the MySQL connection 
+cursor.close()
+conn.close()
+return print("code corrected")
--- src/pages/Code/Code.jsx
+++ src/pages/Code/Code.jsx
@@ -5,8 +5,9 @@ import Header from "../../components/Header/";
 import Tab from "../../components/Tab/";
 import Tabs from "../../components/Tabs/";
 import Output from "../../components/Output/";
+import CodeAction from "./CodeAction";
 import EASYBOT_NORMAL from "../../assets/images/easybot-normal.svg";
-import { useState, createContext, useContext } from "react";
+import { useState, createContext } from "react";
 import axios from 'axios';
 
 // Create Context object
@@ -15,18 +16,26 @@ export const CodeContext = createContext();
 const Code = () => {
   const [inputText, setInputText] = useState("");
 
+  const [playIsSelected, setPlayIsSelected] = useState(false);
+  const [pauseIsSelected, setPauseIsSelected] = useState(false);
+  const [restartIsSelected, setRestartIsSelected] = useState(false);
+
   const handleChange = (event) => {
     setInputText(event.target.value);
   };
 
   const handleCopyToAPI = () => {
-    axios.post('http://127.0.0.1:5000/api/send', {data: inputText})
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-      });
+    // Verifique se o inputText não está vazio antes de fazer a solicitação
+    if (inputText.trim() !== '') {
+      axios.post('http://127.0.0.1:5000/api/send', { data: inputText })
+        .then(response => {
+          console.log(response.data);
+          console.log("ENVIADO");
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+    }
   };
 
   // Value to be passed to Provider must be an object
@@ -48,7 +57,31 @@ const Code = () => {
           </Style.HeaderContainer>
           <Style.Content>
             <Style.ContentBody>
-                <CodeButtons />
+              <Style.CodeActions>
+                <CodeAction
+                  actionIcon="ri-play-line"
+                  actionTitle="Iniciar"
+                  type="button"
+                  onClick={() => {
+                    setPlayIsSelected((prev) => !prev);
+                    handleCopyToAPI(); // Chame a função de envio quando o botão "Iniciar" for clicado
+                  }}
+                  actionIsSelected={playIsSelected}
+                />
+
+                <CodeAction
+                  actionIcon="ri-pause-fill"
+                  actionTitle="Pausar"
+                  onClick={() => setPauseIsSelected((prev) => !prev)}
+                  actionIsSelected={pauseIsSelected}
+                />
+                <CodeAction
+                  actionIcon="ri-restart-line"
+                  actionTitle="Reiniciar"
+                  onClick={() => setRestartIsSelected((prev) => !prev)}
+                  actionIsSelected={restartIsSelected}
+                />
+              </Style.CodeActions>
               <Style.InputText placeholder="Insira o código" onChange={handleChange} />
             </Style.ContentBody>
             <Output />

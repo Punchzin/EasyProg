5faadc61ef6e8b4187b4e9989fe88beed1c3dbdc
Punchzin <gabrielssscavalcante@gmail.com>
2023-10-30 11:57:44 +0000

    bug fix routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,16 +4,16 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Apresentation from '../pages/Apresentation';
 import Authentication from '../pages/Authentication';
 import Code from "../pages/Code";
-import Login from '../pages/Authentication/login';
-import Register from '../pages/Authentication/register';
+// import Login from '../pages/Authentication/login';
+// import Register from '../pages/Authentication/register';
 
 const RoutesApp = () => {
   return (
      <Routes>
        <Route path='/' element={<Navigate to="/overview" />}/>
        <Route path='/login' element={<Authentication />}/>
-       <Route path='/auth' element={<Login />}/>
-       <Route path='/register' element={<Register />}/>
+       {/* <Route path='/auth' element={<Login />}/>
+       <Route path='/register' element={<Register />}/> */}
        <Route path='/overview' element={<Apresentation />}/>
        <Route path='/code' element={<Code />}/>
      </Routes>

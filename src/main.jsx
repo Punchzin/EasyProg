7e8aaf7136bc26f85622ad6be6eff2a6fe7e891d
Punchzin <gabrielssscavalcante@gmail.com>
2023-11-06 22:08:46 +0000

    error fix main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
         <RoutesApp />
       </BrowserRouter>
     </AuthProvider>

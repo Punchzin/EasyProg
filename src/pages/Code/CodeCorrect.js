30d08c58dd8e23ebf9a4fea570074dd4b6d7c106
Emilly Silva <emilly.silva117@etec.sp.gov.br>
2023-11-08 16:17:04 +0000

    gitpod config

--- src/pages/Code/CodeCorrect.js
+++ src/pages/Code/CodeCorrect.js
@@ -1,16 +1,18 @@
-import { OpenAi } from 'openai';    
-    
-    // openai connection
-const openai = new OpenAi({apiKey: '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', dangerouslyAllowBrowser: true});
-    async function correctCode(inputText) {
-      const prompt = `correct and explain the errors of the following incorrect python code:\n\n${inputText}`;
-    
-      const response = await openai.complete({
-        engine: 'text-davinci-003', // or 'gpt-3.5-turbo'
-        prompt: prompt,
-        maxTokens: 500,
-      });
-      console.log(response.choices[0].text);
-    }
+import { OpenAi } from 'openai';
+import { InputText } from './Code.styles';
+
+// openai connection
+const openai = new OpenAi({ apiKey: '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', dangerouslyAllowBrowser: true });
+async function correctCode(inputText) {
+  const prompt = `correct and explain the errors of the following incorrect python code:\n\n${inputText}`;
+
+  const response = await openai.complete({
+    engine: 'text-davinci-003', // or 'gpt-3.5-turbo'
+    prompt: prompt,
+    maxTokens: 500,
+  });
+  console.log(response.choices[0].text);
+}
 
 // exporta pro react e chama a função, ta tudo no forefront.
+export default correctCode;

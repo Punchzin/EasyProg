51c90afec90ff90e4817f6c2ba11ff27b6c2b646
Gabriel Soares <105940092+Punchzin@users.noreply.github.com>
2023-11-12 00:48:56 -0300

    Update Authentication.jsx

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,45 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { CircularProgress } from "@mui/material";
 import * as Style from "./Authentication.styles";
 import EASYPROG_LOGO from "../../assets/images/easyprog-logo.svg";
 import EASYPROG_ROBOT_SMILE from "../../assets/images/easybot-smile.svg";
 import EASYPROG_ROBOT_DUBIOUS from "../../assets/images/easybot-dubious.svg";
-import { auth } from "../../services/firebaseConfig";
+import useAuth from "../../hooks/useAuth";
 
 const Authentication = () => {
   const navigate = useNavigate();
 
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [
-    createUserWithEmailAndPassword,
-    user,
-    loading,
-    error,
-  ] = useCreateUserWithEmailAndPassword(auth);
-      function handleSignOut(e) {
-        e.preventDefault();
-        createUserWithEmailAndPassword(email, password);
-      }
-
-
-  const [screen, setScreen] = useState(0);
-  const [usernameField, setUsernameField] = useState("");
-  const [passwordField, setPasswordField] = useState("");
-  const [confirmPasswordField, setConfirmPasswordField] = useState("");
-
   const [passwordView, setPasswordView] = useState(false);
   const [passwordConfirmView, setPasswordConfirmView] = useState(false);
 
-  const isRegister = screen == 1;  
+  const {
+    CanLogin,
+    CanRegister,
+    handleChangeForm,
+    setIsRegister,
+    isRegister,
+    isLoading,
+    setIsLoading,
+    handleRegister,
+    handleLogin,
+    isFormEmpty,
+  } = useAuth();
+
+  const handleSubmit = async () => {
+    try {
+      setIsLoading(true);
+      if (isRegister) {
+        await handleRegister();
+        navigate('/overview');
+      } else {
+        await handleLogin();
+        navigate('/overview');
+      }
+    } catch(error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const AUTHENTICATION_TITLE = !isRegister ? "Authentication" : "Registro";
-  const AUTHENTICATION_DESCRIPTION = !isRegister ? "Autentique-se" : "Registre-se";
+  const AUTHENTICATION_DESCRIPTION = !isRegister
+    ? "Autentique-se"
+    : "Registre-se";
   const BUTTON_TEXT = !isRegister ? "Cadastrar-se" : "Fazer login";
   const EASYBOT_IMAGE = !isRegister
     ? EASYPROG_ROBOT_SMILE
@@ -47,13 +56,14 @@ const Authentication = () => {
 
   return (
     <Style.Main>
+      <Style.GlobalStyles />
       <Style.Container>
         <Style.AuthenticationWrapper>
           <Style.AuthenticationBackground>
             <Style.Form>
               <Style.FormHeader>
                 <Style.FormLogo>
-                  <img src={EASYPROG_LOGO} alt="logo EasyProg"/>
+                  <img src={EASYPROG_LOGO} alt="logo EasyProg" />
                 </Style.FormLogo>
                 <Style.FormHeaderText>
                   <p>EASYPROG</p>
@@ -64,42 +74,43 @@ const Authentication = () => {
                 <Style.FormAuthentication>
                   <Style.FormAuthenticationText>
                     <Style.Robot>
-                    <img src={EASYBOT_IMAGE} alt="EasyBot" onClick={() => navigate("/overview")}/>
+                      <img
+                        src={EASYBOT_IMAGE}
+                        alt="EasyBot"
+                      />
                     </Style.Robot>
                     <h1>{AUTHENTICATION_TITLE}</h1>
                     <p>
-                      {AUTHENTICATION_DESCRIPTION} na plataforma para utilizar a{" "}
+                      {AUTHENTICATION_DESCRIPTION} na plataforma para utilizar a
                       <strong>ferramenta</strong>.
                     </p>
                   </Style.FormAuthenticationText>
                   <Style.FormBody>
                     <Style.InputWrapper>
-                      <Style.InputLabel htmlFor="email">
-                        Email
-                      </Style.InputLabel>
                       <Style.InputItem>
-                        <i className="ri-user-line"></i>
+                        <i className="ri-at-line"></i>
                         <input
                           type="text"
-                          placeholder="Email"
+                          placeholder="E-mail"
                           id="email"
-                          onChange={(e) => setEmail(e.target.value)}
+                          onChange={(e) =>
+                            handleChangeForm("email", e.target.value)
+                          }
+                          required
                         />
                       </Style.InputItem>
                     </Style.InputWrapper>
                     <Style.InputWrapper>
-                      <Style.InputLabel
-                        htmlFor="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      >
-                        Senha
-                      </Style.InputLabel>
                       <Style.InputItem>
                         <i className="ri-lock-2-line"></i>
                         <input
                           type={!passwordView ? "password" : "text"}
                           placeholder="Password"
+                          onChange={(e) =>
+                            handleChangeForm("password", e.target.value)
+                          }
                           id="password"
+                          required
                         />
                         <Style.InputIconButton
                           onClick={() => setPasswordView((prev) => !prev)}
@@ -113,57 +124,74 @@ const Authentication = () => {
                       </Style.InputItem>
                     </Style.InputWrapper>
                     {isRegister && (
-                      <Style.InputWrapper>
-                        <Style.InputLabel htmlFor="password">
-                          Confimar senha
-                        </Style.InputLabel>
-                        <Style.InputItem>
-                          <i className="ri-lock-2-line"></i>
-                          <input
-                            type={!passwordConfirmView ? "password" : "text"}
-                            placeholder="Confirm password"
-                            id="password"
-														onChange={(e) => setConfirmPasswordField(e.target.value)}
-                          />
-                          <Style.InputIconButton
-                            onClick={() =>
-                              setPasswordConfirmView((prev) => !prev)
-                            }
-                          >
-                            <i
-                              className={
-                                !passwordConfirmView
-                                  ? "ri-eye-line"
-                                  : "ri-eye-off-line"
+                      <React.Fragment>
+                        <Style.InputWrapper>
+                          <Style.InputItem>
+                            <i className="ri-lock-2-line"></i>
+                            <input
+                              type={!passwordConfirmView ? "password" : "text"}
+                              placeholder="Confirm password"
+                              id="password"
+                              onChange={(e) =>
+                                handleChangeForm(
+                                  "passwordConfirm",
+                                  e.target.value
+                                )
                               }
-                            ></i>
-                          </Style.InputIconButton>
-                        </Style.InputItem>
-                      </Style.InputWrapper>
+                            />
+                            <Style.InputIconButton
+                              onClick={() =>
+                                setPasswordConfirmView((prev) => !prev)
+                              }
+                            >
+                              <i
+                                className={
+                                  !passwordConfirmView
+                                    ? "ri-eye-line"
+                                    : "ri-eye-off-line"
+                                }
+                              ></i>
+                            </Style.InputIconButton>
+                          </Style.InputItem>
+                        </Style.InputWrapper>
+                        <Style.InputWrapper>
+                          <Style.InputItem>
+                            <i className="ri-file-user-line"></i>
+                            <input
+                              type="text"
+                              placeholder="Primeiro nome"
+                              id="name"
+                              onChange={(e) =>
+                                handleChangeForm("name", e.target.value)
+                              }
+                              required
+                            />
+                          </Style.InputItem>
+                        </Style.InputWrapper>
+                      </React.Fragment>
                     )}
-                    {/* {!isRegister && (
-                      <Style.FormCheck>
-                        <input
-                          type="checkbox"
-                          name="KeepConnected"
-                          id="KeepConnected"
-                        />
-                        <label htmlFor="KeepConnected">
-                          <i className="ri-check-line"></i>
-                        </label>
-                        <p>Manter-se conectado</p>
-                      </Style.FormCheck>
-                    )} */}
                   </Style.FormBody>
                 </Style.FormAuthentication>
                 <Style.FormButtons>
                   <Style.ButtonRegister
-                    onClick={() => setScreen((prev) => !prev)}
+                    onClick={() => setIsRegister((prev) => !prev)}
                   >
                     {BUTTON_TEXT}
                   </Style.ButtonRegister>
-                
-                  <Style.ButtonContinue onClick={handleSignOut}>Continuar</Style.ButtonContinue>
+                  <Style.ButtonContinue
+                    type="submit"
+                    disabled={isLoading || (isRegister && !CanRegister) || (!isRegister && !CanLogin)}
+                    onClick={() => handleSubmit()}
+                  >
+                    {isLoading ? (
+                      <CircularProgress
+                        size={16}
+                        classes={{ colorPrimary: "CircularProgressColor" }}
+                      />
+                    ) : (
+                      "Continuar"
+                    )}
+                  </Style.ButtonContinue>
                 </Style.FormButtons>
               </Style.FormWrapper>
             </Style.Form>

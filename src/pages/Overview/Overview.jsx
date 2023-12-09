eaedd3a8f7a6afcc339c76b7eff039bef60908a2
EmySilv <emiisilva1978@gmail.com>
2023-12-09 17:53:25 -0300

    feat: changes

--- src/pages/Overview/Overview.jsx
+++ src/pages/Overview/Overview.jsx
@@ -27,15 +27,7 @@ const Overview = () => {
             <Style.Wrapper>
               <Chooser />
               <Style.UseMode>
-                <iframe
-                  width="100%"
-                  height="100%"
-                  src="https://www.youtube.com/embed/zHcjYG3BRnc?autoplay=0&mute=1&controls=0&loop=1&playlist=zHcjYG3BRnc"
-                  title="Tutorial de Uso - EasyProg"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"
-                  allowfullscreen
-                  style={{ border: "none" }}
-                />
+                
               </Style.UseMode>
             </Style.Wrapper>
           </Style.ContentBody>
